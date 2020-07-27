0ae8684520c76944bfc578a62ce1f70ddfbc15a7
tayyaba <tayabaali94@hotmail.com>
2020-07-28 03:41:00 +0500

    Updates

---
+++ precache-manifest.33ba951cd1853f724b9f6e1e5f542d97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e5b3b35d68c796dfc9fa20321f4a86",
+    "url": "/todolistapp/index.html"
+  },
+  {
+    "revision": "dffd16e96286c6b43148",
+    "url": "/todolistapp/static/css/main.e3a398fb.chunk.css"
+  },
+  {
+    "revision": "919263bab6c378cd1137",
+    "url": "/todolistapp/static/js/2.d24772a2.chunk.js"
+  },
+  {
+    "revision": "e04edb4657220a3d610d2b4ad704abab",
+    "url": "/todolistapp/static/js/2.d24772a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dffd16e96286c6b43148",
+    "url": "/todolistapp/static/js/main.fa6b5163.chunk.js"
+  },
+  {
+    "revision": "035cc342d3baf8948861",
+    "url": "/todolistapp/static/js/runtime-main.650bca86.js"
+  }
+]);
